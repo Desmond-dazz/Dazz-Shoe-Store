d85196bd37ee1b1def67546905669258c93bdc3d
Dazz <dazz@gmail.com>
2026-09-08 09:42:05 +0200

    Initial commit of header and hero section

---
+++ script.js
@@ -0,0 +1,12 @@
+const burgerMenu = document.querySelector(".burger-menu");
+const mobileNavigation = document.querySelector(".navigation-mobile");
+let isNavVisble = false;
+burgerMenu.addEventListener("click", () => {
+  if (isNavVisble == false) {
+    mobileNavigation.style.transform = "scaleX(1)";
+    isNavVisble = true;
+  } else {
+    mobileNavigation.style.transform = "scaleX(0)";
+    isNavVisble = false;
+  }
+});
